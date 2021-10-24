e1124abd2e11784fe9374beddbe330342856b8e1
AlexE1998 <aernault44@gmail.com>
2021-10-25 00:51:57 +0200

    commit PlayerServiceTest

---
+++ cdc-api/src/test/java/com/api/cdcapi/services/PlayerServiceTest.java
@@ -0,0 +1,30 @@
+package com.api.cdcapi.services;
+
+import com.api.cdcapi.business.*;
+import com.api.cdcapi.business.enums.ArmorType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+public class PlayerServiceTest {
+
+    @Autowired
+    PlayerService service;
+
+    @Test
+    void check_player_created_with_age_over_sixteen() {
+
+        Player player = service.createPlayer("MELENCHON", "Jean-Luc", new Category("Poids Moyen", 75, 80),
+                new Weapon("Marteau"), new Armor(ArmorType.GAMBISON), 70, 10, true);
+
+        Assertions.assertNotNull(player);
+
+    }
+
+}

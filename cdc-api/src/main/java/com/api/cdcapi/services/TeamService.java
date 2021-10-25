a9f4b2cf107094bb5a129a86484a7b6e1b70e1a2
Thibault Doucet <thibault.doucet44@gmail.com>
2021-10-25 11:16:35 +0200

    :sparkles: Add weights category checking for teams

--- cdc-api/src/main/java/com/api/cdcapi/services/TeamService.java
+++ cdc-api/src/main/java/com/api/cdcapi/services/TeamService.java
@@ -32,4 +32,20 @@ public class TeamService {
         System.out.println(team2.toString());
     }
 
+    public boolean areWeightsEven(Team team1, Team team2, List<Category> categories) {
+        Category selected = null;
+        double team1Mean = team1.getPlayersWeightMean();
+        double team2Mean = team2.getPlayersWeightMean();
+
+        for (Category category : categories) {
+            if (category.getLower_value() <= team1Mean && category.getHigher_value() >= team1Mean) {
+                selected = category;
+                break;
+            }
+        }
+
+        if (selected == null) return false;
+        return team2Mean >= selected.getLower_value() && team2Mean <= selected.getHigher_value();
+    }
+
 }
